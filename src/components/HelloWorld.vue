a9f0e32b9c9b9de4ff4e81d857d53b86926edf6b
liubai-github <1319892950@qq.com>
2020-02-07 15:14:51 +0800

    yc commit2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -89,8 +89,10 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
+      role: 0,
+      date: '2020-02-10'
       tips: 'hellow',
-      date: '2020-02-07'
+      color: 'red'
     }
   }
 }
@@ -98,18 +100,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
